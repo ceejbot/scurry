84d4b872b75f14ab09cb7f6ef378a30f8efe10d2
C J Silverio <ceejceej@gmail.com>
2013-07-12 22:21:48 -0700

    A tiny bit more work; the database module really ought to export functions that the restful endpoints modules uses to do its get/sets.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Pull all the pieces in lib together to construct a server
 
+var
+	storage = require('./lib/database'),
+	endpoints = require('./lib/endpoints'),
+	mesh = require('./lib/mesh')
+	;
+
+// optimist
+
