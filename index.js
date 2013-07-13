f8ecaf2f35fadf119f1291b23deaa34f9a4f1285
C J Silverio <ceejceej@gmail.com>
2013-07-13 16:36:58 -0700

    Invoke it this way: node index.js -p 2000 -g 4114 -d ./db
    It runs and claims to join things, but I have not yet run two of them...

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 // Pull all the pieces in lib together to construct a server
 
 var
-	storage   = require('./lib/localnode'),
-	remote    = require('./lib/remotenode'),
-	endpoints = require('./lib/endpoints'),
-	mesh      = require('./lib/mesh')
+	Mesh      = require('./lib/mesh')
+	optimist  = require('optimist')
+		.usage('Run a single node in the mesh.\nUsage: $0 --id=node-one -p 3000 -g 4114 --dbpath=./db')
+		.alias('d', 'dbpath')
+		.describe('d', 'path to store leveldb files')
+		.alias('g', 'gossip')
+		.describe('g', 'port for the gossip traffic')
+		.alias('p', 'port')
+		.describe('p', 'port for the RESTify service to listen on')
+		.alias('i', 'id')
+		.describe('i', 'ID of this node; must be unique in the mesh')
+		.demand(['d', 'g', 'p'])
 	;
 
-// optimist
+var opts =
+{
+	id:         optimist.argv.id,
+	dbpath:     optimist.argv.dbpath,
+	gossipport: optimist.argv.gossip,
+	port:       optimist.argv.port
+};
+var mesh = new Mesh(opts);
 
-// create mesh; get settings from mesh
-// create restify server with endpoints & start it listening
-// join mesh
-// update lightcycle
+
+process.on('SIGINT', mesh.disconnect.bind(mesh));
+
+mesh.connect()
+.then(function()
+{
+	mesh.join();
+})
+.fail(function(err)
+{
+	console.log(err);
+	process.exit(1);
+})
+.done();
