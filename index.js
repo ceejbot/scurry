d3184e71486f3301089396d244e9b6dd698b5092
C J Silverio <ceejceej@gmail.com>
2013-07-13 08:05:36 -0700

    Implemented the endpoints in terms of the storage module, so the rest server doesn't need to have a clue if its requests are being fulfilled locally or not.
    Wrote the local fulfillment part of the storage methods, with promises
    because I can't go back to no flow control.
    
    More notes about upcoming work.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Pull all the pieces in lib together to construct a server
 
 var
-	storage = require('./lib/database'),
+	storage = require('./lib/storage'),
 	endpoints = require('./lib/endpoints'),
 	mesh = require('./lib/mesh')
 	;
 
 // optimist
 
+// create mesh; get settings from mesh
+// create restify server with endpoints & start it listening
+// join mesh
+// update lightcycle
