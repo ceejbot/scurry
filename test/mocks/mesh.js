0746ca948d3169da3e8680cae492914aefa4074d
C J Silverio <ceejceej@gmail.com>
2013-07-19 08:38:03 -0700

    Stubbed out a set of tests for the Restify server in endpoints. Wrote mock mesh and node objects to use as storage for the endpoint tests.

---
+++ test/mocks/mesh.js
@@ -0,0 +1,69 @@
+var
+	crypto     = require('crypto'),
+	Lightcycle = require('light-cycle'),
+	P          = require('p-promise')
+	;
+
+var MockMesh = module.exports = function MockMesh(opts)
+{
+	var count = opts.count || 1;
+	this.cycle = new Lightcycle({ seed: 0xcafed00d, size: count });
+
+	for (var i = 0; i < count; i++)
+	{
+		var node = new MockNode(String(i));
+		this.cycle.add(node, node.id);
+	}
+};
+
+MockMesh.prototype.locate = function(bucket, key)
+{
+	var node = this.cycle.locate(bucket + '/' + id);
+	return node;
+};
+
+
+function MockNode(id)
+{
+	this.id = id;
+	this.store = {};
+}
+MockMesh.MockNode = MockNode;
+
+MockNode.prototype.get = function(bucket, id)
+{
+	if (this.store[bucket])
+		return P(this.store[bucket][id]);
+
+	return P(null);
+};
+
+MockNode.prototype.set = function(bucket, id, value, ttl)
+{
+	if (!this.store[bucket])
+		this.store[bucket] = {};
+
+	var crc = crypto.createHash('md5');
+	var item =
+	{
+		payload: value,
+		ts:      Date.now(),
+		etag:    crc.update(JSON.stringify(value)).digest('hex')
+	};
+	this.store.bucket = item;
+
+	return P('OK');
+};
+
+MockNode.prototype.del = function(bucket, id)
+{
+	if (this.store[bucket])
+		delete this.store[bucket][id];
+
+	return P('OK');
+};
+
+MockNode.prototype.keys = function(bucket)
+{
+	return P(Object.keys(this.store[bucket]));
+};
--- test/test-03-endpoints.js
+++ test/test-03-endpoints.js
@@ -7,18 +7,56 @@ var
 	should     = chai.should()
 	;
 
-var endpoints = require('../lib/endpoints');
+var endpoints = require('../lib/endpoints'),
+	MockMesh = require('./mocks/mesh')
+	;
+
+
+var mesh = new MockMesh(5);
+
 
 describe('REST api', function()
 {
-	it('createServer() creates a server');
-	it('the server listens on the passed-in port');
-	it('has tests for each route');
-	it('has tests for bogus requests, e.g., bad form data');
-	it('tests for various middleware');
-	it('GET sends back an ETag header');
-	it('GET sends back the same ETag header for unchanged data');
-	it('GET sends a last-modified header');
-	it('demands JSON data');
+	describe('createServer()', function()
+	{
+		it('createServer() returns a configured restify server');
+		it('the server listens on the passed-in port');
+		it('has tests for bogus requests, e.g., bad form data');
+		it('tests for various middleware');
+	});
+
+	describe('GET /:bucket', function()
+	{
+		it('key streaming is implemented!');
+	});
+
+	describe('POST /:bucket', function()
+	{
+		it('demands JSON data');
+
+	});
+
+	describe('PUT /:bucket/:id', function()
+	{
+		it('demands JSON data');
+
+	});
+
+	describe('GET /:bucket/:id', function()
+	{
+		it('GET sends back an ETag header');
+		it('GET sends back the same ETag header for unchanged data');
+		it('GET sends a last-modified header');
+	});
+
+	describe('HEAD /:bucket/:id', function()
+	{
+		it('has tests');
+	});
+
+	describe('DEL /:bucket/:id', function()
+	{
+		it('has tests');
+	});
 
 });
